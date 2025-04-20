ad0382914378eccf357b1812977401e5b79fb176
Arshia Faraji <arshiafaraji22@gmail.com>
2025-04-20 17:05:22 +0330

    update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,15 @@
 import React from "react";
-import { GoMarkGithub } from "react-icons/go";
+import { IoLogoGithub } from "react-icons/io";
 
 function Footer() {
   return (
     <footer>
-      <a href="https://github.com/Arsh1a/crypto-notifier" target="_blank" rel="noreferrer">
-        <GoMarkGithub />
+      <a
+        href="https://github.com/Arsh1a/crypto-notifier"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <IoLogoGithub />
       </a>
     </footer>
   );
